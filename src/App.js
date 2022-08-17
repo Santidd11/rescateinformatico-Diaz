7d50501768a3d7ed0466ad0af5eec7ee19e5008e
Santiago Diaz <santidd11@gmail.com>
2022-08-16 21:04:28 -0300

    Desafio 01

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Página web Ecommerce de servicio técnico computacional.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hecha con React
         </a>
       </header>
     </div>
