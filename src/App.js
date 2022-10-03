f415395e838ea4ba2d3e0873300e3178c7b82838
Santiago Diaz <santidd11@gmail.com>
2022-10-03 14:28:51 -0300

    firebase 1c

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Routes>
             <Route path='/' element={<ItemListContainer />} />
             <Route path='/category/:categoryId' element={<ItemListContainer />} />
-            <Route path='/detail/:itemId' element={<ItemDetailContainer />} />
+            <Route path='/detail/:id' element={<ItemDetailContainer />} />
             <Route path='/cart' element={<Cart/>} />
           </Routes>
         </BrowserRouter>
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,32 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import ItemDetail from '../ItemDetail/ItemDetail'
 import { useParams } from 'react-router-dom';
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, documentId } from "firebase/firestore";
 import { db } from '../../FireBaseConfig'; 
 
 const ItemDetailContainer = () => {
 
     const[items, setItems] = useState([])
     const[loading, setLoading] = useState(true)
-    const { categoryId } = useParams()
+    const { id } = useParams();
 
+    const getItems = async () => {
+            const q = query(collection(db, 'stock-computadoras'), where(documentId(), '==', id));
+            const docs = [];
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                    docs.push({...doc.data(), id: doc.id})
+            });
+            setItems(docs[0]);
+    };
 
     useEffect(() => {
-            const getItems = async () => {
-                const q = query(collection(db, 'stock-computadoras'));
-                const docs = [];
-                const querySnapshot = await getDocs(q);
-                querySnapshot.forEach((doc) => {
-                        docs.push({...doc.data(), id: doc.id})
-                });
-                setItems(docs);
-                if (items.length > 0){
-                    setLoading(false);
-                }
-                console.log(items)
-        };
             getItems();
-    }, [])
+            setTimeout(() => {
+                setLoading(false);
+            }, 1000);
+    }, [id])
 
 
 
@@ -39,7 +38,7 @@ const ItemDetailContainer = () => {
                             <h1 className='cargando'>CARGANDO...</h1>
                             :
                             <div className="UserSection container">
-                                    <div className='row justify-content-md-center'>
+                                    <div className='row justify-content-md-center hola'>
                                         <ItemDetail {...items}/>
                                     </div>
                             </div>
